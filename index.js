32cab3b0ab45450f6e7b0ac36ecd71dc3ca8b1a0
Lanu135 <143382856+Lanu135@users.noreply.github.com>
2023-10-22 10:47:44 +0200

    added Eventlistener to Button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Setup for Firebase Database
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://realtime-database-ac20c-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -11,8 +11,18 @@ const database = getDatabase(app)
 const endorsementInDatabase = ref(database, "endorsement")
 
 // Variables to catch up Elements easier
-const endorsementEl = document.getElementById("endorsement-el")
+const commentEl = document.getElementById("comment-el")
 const fromEl = document.getElementById("from-el")
 const toEl = document.getElementById("to-el")
 const publishBtn = document.getElementById("publish-btn")
 const endorsementList = document.getElementById("endorsement-list")
+
+
+
+publishBtn.addEventListener("click", function() {
+  const commentValue = commentEl.value
+
+  push(endorsementInDatabase, commentValue)
+  
+
+})
