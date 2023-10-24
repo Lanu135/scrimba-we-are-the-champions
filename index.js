a43fd6c426718f6e157f6d1fb940bae016cc2674
Lanu135 <143382856+Lanu135@users.noreply.github.com>
2023-10-24 19:52:47 +0200

    implement from and to functionality

--- index.js
+++ index.js
@@ -12,15 +12,19 @@ const commentInDatabase = ref(database, "comment")
 
 // Variables to catch up Elements easier
 const commentEl = document.getElementById("comment-el")
-// const fromEl = document.getElementById("from-el")
-// const toEl = document.getElementById("to-el")
+const fromEl = document.getElementById("from-el")
+const toEl = document.getElementById("to-el")
 const publishBtn = document.getElementById("publish-btn")
 const commentListEl = document.getElementById("comment-list")
 
 
 // pushs value of commentEl to Firebase and clears the commentEl
 publishBtn.addEventListener("click", function() {
-  const commentValue = commentEl.value
+  const commentValue = {
+    from: fromEl.value,
+    comment: commentEl.value,
+    to: toEl.value
+  }
 
   push(commentInDatabase, commentValue)
   clearInputFields()
@@ -33,20 +37,31 @@ onValue(commentInDatabase, function(snapchat) {
 
   for (let i = 0; i < commentsArray.length; i++) {
     let currentComment = commentsArray[i]
-    let currentCommentID = currentComment[0]
+    // not used yet let currentCommentID = currentComment[0]
     let currentCommentValue = currentComment[1]
-    appendCommentToCommentListEl(currentCommentValue)
+    let from = currentCommentValue.from
+    let comment = currentCommentValue.comment
+    let to = currentCommentValue.to
+    appendCommentToCommentListEl(comment, from, to)
   }
   
 
 })
 
 
-function appendCommentToCommentListEl(comment) {
-  let newEl = document.createElement("li")
-  newEl.textContent = comment
+function appendCommentToCommentListEl(comment, from, to) {
+  let newCommentEl = document.createElement("li")
+  let newFromEl = document.createElement("p")
+  let newToEl = document.createElement("p")
+  newCommentEl.textContent = comment
+  newFromEl.textContent = `From ${from}`
+  newToEl.textContent = `To ${to}`
 
-  commentListEl.append(newEl) 
+
+  
+  commentListEl.append(newCommentEl)
+  newCommentEl.appendChild(newToEl) 
+  newCommentEl.prepend(newFromEl)
 }
 
 function clearCommentListEl() {
@@ -55,6 +70,6 @@ function clearCommentListEl() {
 
 function clearInputFields() {
   commentEl.value = ""
-  // fromEl.value = ""
-  // toEl.value = ""
+   fromEl.value = ""
+   toEl.value = ""
 }
