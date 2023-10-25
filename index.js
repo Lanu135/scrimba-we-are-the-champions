f9754c4349ccd211b0167dd193052960a1a49291
Lanu135 <143382856+Lanu135@users.noreply.github.com>
2023-10-25 18:33:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-
 import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-  // using secret for Firebase URL see Repositry settings
-  databaseURL: FIREBASEURL
+
+  databaseURL: "https://realtime-database-ac20c-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
